568f830ea49b4c30d2502250e2dc43f4a3e939d8
Grygory Ksheminsky <maroon785@gmail.com>
2022-05-20 21:16:56 +0300

    working with index state

--- src/Review.js
+++ src/Review.js
@@ -3,6 +3,9 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
+  const [index, setIndex] = useState(0);
+  console.log(people);
+  const { id, image, job, name, text } = people[index];
   return <h2>review component</h2>;
 };
 
